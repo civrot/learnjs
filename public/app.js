ddd6fcb08facbbfab4ba62e7e25c61111e66e08e
Victor Asteinza <vic+github@civrot.org>
2018-05-25 00:28:36 -0700

    Add link to next problem

--- public/app.js
+++ public/app.js
@@ -13,6 +13,10 @@ learnjs.problems = [
   }
 ];
 
+learnjs.template = function(name) {
+  return $('.templates .' + name).clone();
+}
+
 learnjs.applyObject = function(obj, elem) {
   for (var key in obj) {
     elem.find('[data-name="' + key + '"]').text(obj[key]);
@@ -26,9 +30,21 @@ learnjs.flashElement = function(elem, content) {
   });
 }
 
+learnjs.buildCorrectFlash = function (problemNum) {
+  var correctFlash = learnjs.template('correct-flash');
+  var link = correctFlash.find('a')
+  if (problemNum < learnjs.problems.length) {
+    link.attr('href', '#problem-' + (problemNum +1));
+  } else {
+    link.attr('href', '');
+    link.text("You're Finished!");
+  }
+  return correctFlash;
+}
+
 learnjs.problemView = function(data) {
   var problemNumber = parseInt(data, 10);
-  var view = $('.templates .problem-view').clone();
+  var view = learnjs.template('problem-view');
   var problemData = learnjs.problems[problemNumber - 1];
   var resultFlash = view.find('.result');
 
@@ -40,7 +56,8 @@ learnjs.problemView = function(data) {
 
   function checkAnswerClick() {
     if (checkAnswer()) {
-      learnjs.flashElement(resultFlash, 'Correct!');
+      var flashContent = learnjs.buildCorrectFlash(problemNumber);
+      learnjs.flashElement(resultFlash, flashContent);
     } else {
       learnjs.flashElement(resultFlash, 'Incorrect!');
     }
